fc5e8a0be17c73eb97b7a28492301e0857a06814
veena694 <119649485+veena694@users.noreply.github.com>
2024-07-27 11:20:11 +0530

    auth.controller.js

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -1,8 +1,18 @@
 const user = require("../models/userModel");
 
-async function getMe(req, res) {}
+async function getMe(req, res) {
+  try {
+        const user = await User.findById(req.usertoken).select("-password"); // Exclude passwordHash from response
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        res.json(user);
+    } catch (error) {
+        res.status(500).json({ message: "Error fetching user details", error });
+    }
+}
 
-// async function login(req, res) {}
+
 
 module.exports = {
   getMe
